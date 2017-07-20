ffc41fae1665d607936f9263738bd39d841e10b9
MeganMFisher <meganmichelle318@gmail.com>
2017-07-20 08:21:40 -0600

    getMessages working!

---
+++ server/controller.js
@@ -0,0 +1,35 @@
+module.exports = {
+   
+    getAllMessages: (req, res) => {
+        req.app.get('db').get_messages().then(foundMessages => {
+            res.send(foundMessages);
+        }).catch(err => {
+            res.status(500).send(err)
+        })
+    },
+
+    createMessage: (req, res) => {
+        var stuff = [
+            req.body.message,
+            req.body.id
+        ]
+
+        req.app.get('db').create_message(stuff).then((response) => {
+            res.send('Posted')
+        })
+
+    }
+
+    // createMessage: (app, messageData) => {
+    //     const db = app.get('db')
+    //     console.log(db)
+    //     const message = messageData.message_body;
+    //     const sender_id = messageData.sender_id
+    //     console.log(message);
+    //     return db.create_message([message, sender_id]).then(resp => {
+    //         return resp
+    //         console.log(resp)
+    //     })
+    // },
+
+}
---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      massive = require('massive'),
+      config = require('./config.js'),
+      cors = require('cors'),
+      http = require('http'),
+      io = require('socket.io')//(server, { serveClient: false });
+
+massive(config.database).then(db => {
+    app.set('db', db)
+}).catch((err) => {
+    console.log(err)
+})
+
+var port = 3000;
+
+var app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const controller = require('./controller.js')
+
+// io.on('connection', socket => {
+//   console.log('A user connected')
+
+//   socket.on('user_connected', data => {
+//     socket.broadcast.emit('user_connected', { data })
+//     socket.emit('user_connected', { data })
+//   })
+
+//   socket.on('chat_message', data => {
+//     controller.createMessage(app, data).then(resp => {
+//       socket.broadcast.emit('chat_message', data)
+//       socket.emit('chat_message', data)
+//     })
+//       .catch(err => {
+//         socket.emit('error', err.message)
+//       })
+//     console.log(data)
+
+//   })
+
+//   socket.on('chat', data => {
+//     socket.broadcast.emit('chat', data);
+//   })
+
+//   //Send a message after a timeout of 4seconds
+//   setTimeout(function () {
+//     socket.send('Sent a message 4seconds after connection!');
+//   }, 4000);
+// });
+
+
+app.get('/api/messages/', controller.getAllMessages)
+app.post('/api/message/', controller.createMessage)
+
+
+app.listen(port, function() {
+  console.log("Started server on port", port);
+});

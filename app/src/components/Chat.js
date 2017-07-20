6e888c29b569e7680cb95cc493a0bc4dadd2b6b0
MeganMFisher <meganmichelle318@gmail.com>
2017-07-20 10:39:52 -0600

    fixed server

--- app/src/components/Chat.js
+++ app/src/components/Chat.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import io from 'socket.io-client'
 
+
+const socket = io('http://localhost:3010')
 
 export default class Chat extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            messages: [],
+        }
+    }
+
+    componentDidMount() {
+
+    }
 
 
     render() {
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express'),
       massive = require('massive'),
       config = require('./config.js'),
       cors = require('cors'),
-      http = require('http').server(app),
-      io = require('socket.io')(http)
+      server = require('http').createServer(app),
+      io = require('socket.io')(server, { serveClient: false });
 
 massive(config.database).then(db => {
     app.set('db', db)
@@ -12,7 +12,7 @@ massive(config.database).then(db => {
     console.log(err)
 })
 
-var port = 3000;
+var port = 3010;
 
 var app = express()
 app.use(bodyParser.json())
@@ -21,45 +21,45 @@ app.use(cors())
 const controller = require('./controller.js')
 
 
-io.on('connection', function(socket) {
-    console.log('we have a connection!')
-    socket.on('new-message', function(msg){
-        console.log(msg)
-        io.emit('receive-message', msg)
-    })
-})
-
-
-
-// io.on('connection', socket => {
-//   console.log('A user connected')
-
-//   socket.on('user_connected', data => {
-//     socket.broadcast.emit('user_connected', { data })
-//     socket.emit('user_connected', { data })
-//   })
-
-//   socket.on('chat_message', data => {
-//     controller.createMessage(app, data).then(resp => {
-//       socket.broadcast.emit('chat_message', data)
-//       socket.emit('chat_message', data)
+// io.on('connection', function(socket) {
+//     console.log('we have a connection!')
+//     socket.on('new-message', function(msg){
+//         console.log(msg)
+//         io.emit('receive-message', msg)
 //     })
-//       .catch(err => {
-//         socket.emit('error', err.message)
-//       })
-//     console.log(data)
+// })
 
-//   })
 
-//   socket.on('chat', data => {
-//     socket.broadcast.emit('chat', data);
-//   })
 
-//   //Send a message after a timeout of 4seconds
-//   setTimeout(function () {
-//     socket.send('Sent a message 4seconds after connection!');
-//   }, 4000);
-// });
+io.on('connection', socket => {
+  console.log('A user connected')
+
+  socket.on('user_connected', data => {
+    socket.broadcast.emit('user_connected', { data })
+    socket.emit('user_connected', { data })
+  })
+
+  socket.on('chat_message', data => {
+    controller.createMessage(app, data).then(resp => {
+      socket.broadcast.emit('chat_message', data)
+      socket.emit('chat_message', data)
+    })
+      .catch(err => {
+        socket.emit('error', err.message)
+      })
+    console.log(data)
+
+  })
+
+  socket.on('chat', data => {
+    socket.broadcast.emit('chat', data);
+  })
+
+  //Send a message after a timeout of 4seconds
+  setTimeout(function () {
+    socket.send('Sent a message 4seconds after connection!');
+  }, 4000);
+});
 
 
 app.get('/api/messages/', controller.getAllMessages)
